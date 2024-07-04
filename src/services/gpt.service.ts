658d8f96f652ceca8e7a4318152d1e6a14b3de60
Bilbo <stevekalex@gmail.com>
2024-07-05 00:40:19 +0100

    feat: GPT call takes in image

--- src/services/gpt.service.ts
+++ src/services/gpt.service.ts
@@ -1,15 +1,41 @@
 require('dotenv').config();
+var fs = require('fs');
 import OpenAI from "openai";
 
 const openai = new OpenAI();
+
+
+// function to encode file data to base64 encoded string
+const base64Encode = (file) => {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
+
+const base64str = base64Encode('src/services/cat.jpg');
 
 export class GPT {
   static async testRequest() {
     const completion = await openai.chat.completions.create({
       messages: [
-          { "role": "assistant", "content": "Describe Gareth Southgates management style in 5 words"},
+        {
+          "role": "user",
+          "content": [
+            {
+              "type": "text",
+              "text": "Wha is the core object in this image? Return ONLY that word but in Spanish"
+            },
+            {
+              "type": "image_url",
+              "image_url": {
+                "url": `data:image/jpeg;base64,${base64str}`
+              }
+            }
+          ]
+        }
         ],
-      model: "gpt-3.5-turbo"
+      model: "gpt-4o"
     })
     console.log("Result =>", completion.choices[0])
   }
